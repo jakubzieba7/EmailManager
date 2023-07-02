90e9edcd462a5b43977abe294afc2bf0bde27fcc
Jakub Zięba <jakubzieba7@gmail.com>
2023-07-02 15:53:25 +0200

    HomeController and Index view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmailManager.Models.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,13 +12,96 @@ namespace EmailManager.Controllers
     {
         public ActionResult Index()
         {
+            var emails = new List<Email>
+            {
+                new Email
+                {
+                    Id = 1,
+                    MessageSubject="Nowa wiadomość",
+                    MessageBody="Przykładowy tekst tej wiadomości.",
+                    EmailSendDate= DateTime.Now,
+                    Receivers=new List<Receiver>
+                    {
+                        new Receiver
+                        {
+                            Id = 1,
+                            Name="Jarek",
+                            Surname="Kot",
+                            EmailAddress="jarek.kot@mail.com",
+                        },
+                        new Receiver
+                        {
+                            Id = 2,
+                            Name="Dariusz",
+                            Surname="Sowa",
+                            EmailAddress="darek.sowa@mail.com",
+                        }
+                    },
+                    Sender=new Sender
+                    {
+                        Id = 2,
+                        SendersPersonalData=new List<SenderPersonalData>
+                        {
+                            new SenderPersonalData
+                            {
+                                Id=1,
+                                Name="Władek",
+                                Surname="Kłoda"
+                            }
+                        }
+                    }
+                },
+                new Email
+                {
+                    Id = 2,
+                    MessageSubject="Stara wiadomość",
+                    MessageBody="Przykładowy tekst kolejnej wiadomości.",
+                    EmailSendDate= DateTime.Now,
+                    Receivers=new List<Receiver>
+                    {
+                        new Receiver
+                        {
+                            Id=3,
+                            Name="Bogdan",
+                            Surname="Jastrząb",
+                            EmailAddress="bogdan.jastrzab@mail.com",
+                        },
+                        new Receiver
+                        {
+                            Id = 4,
+                            Name="Mariusz",
+                            Surname="Klocek",
+                            EmailAddress="mariusz.klocek@mail.com",
+                        }
+                    },
+                    Sender=new Sender
+                    {
+                        Id=1,
+                        SendersPersonalData=new List<SenderPersonalData>
+                        {
+                            new SenderPersonalData
+                                {
+                                    Id=1,
+                                    Name="Bolesław",
+                                    Surname="Dąb"
+                                }
+                        }
+                    }
+                }
+            };
+
+            return View(emails);
+        }
+
+        public ActionResult Email()
+        {
             return View();
         }
 
         [AllowAnonymous]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Strona o mnie";
 
             return View();
         }
@@ -25,7 +109,7 @@ namespace EmailManager.Controllers
         [AllowAnonymous]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Informacje kontaktowe";
 
             return View();
         }
