73cd19ae481e2d441ad41ddc08299761f24d3876
Jakub Zięba <jakubzieba7@gmail.com>
2023-08-20 12:46:13 +0200

    Generating PDF with Rotativa

---
+++ Controllers/PrintController.cs
@@ -0,0 +1,65 @@
+﻿using EmailManager.Models.Domains;
+using EmailManager.Models.Repositories;
+using Microsoft.AspNet.Identity;
+using Rotativa;
+using Rotativa.Options;
+using System;
+using System.Web.Mvc;
+
+namespace EmailManager.Controllers
+{
+    public class PrintController : Controller
+    {
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private EmailRepository _emailRepository = new EmailRepository();
+
+        public ActionResult EmailToPdf(int id)
+        {
+            var handle = Guid.NewGuid().ToString();
+            var userId = User.Identity.GetUserId();
+            var email = _emailRepository.GetEmail(id, userId);
+
+            TempData[handle] = GetPdfContent(email);
+
+            return Json(new
+            {
+                FileGuid = handle,
+                FileName = $@"Email_{email.Id}.pdf"
+            });
+        }
+
+        private byte[] GetPdfContent(Email email)
+        {
+            var pdfResult = new ViewAsPdf(@"EmailTemplate", email)
+            {
+                PageSize = Rotativa.Options.Size.A4,
+                PageOrientation = Orientation.Portrait
+            };
+
+            return pdfResult.BuildFile(ControllerContext);
+        }
+
+        public ActionResult DownloadEmailPdf(string fileGuid, string fileName)
+        {
+            if (TempData[fileGuid] == null)
+                throw new Exception("Błąd przy próbie eksportu emaila do PDF.");
+
+            var data = TempData[fileGuid] as byte[];
+            return File(data, "application/pdf", fileName);
+        }
+
+        public ActionResult EmailTemplate(int emailId)
+        {
+            var userId = User.Identity.GetUserId();
+            var invoice = _emailRepository.GetEmail(emailId, userId);
+
+            return View(invoice);
+        }
+    }
+}
